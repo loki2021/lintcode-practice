896c03c1312d8f5e4216238bdc458c4e6b3ce9f9
闫飞 <583585752@qq.com>
2021-01-06 15:51:17 +0800

    # 116. 补充一种新的解法

--- src/com/yf/lintcode/binarytree/InvertTree.java
+++ src/com/yf/lintcode/binarytree/InvertTree.java
@@ -19,13 +19,25 @@ public class InvertTree {
     }
 
     public static TreeNode invertTree(TreeNode root) {
-        if(root == null){
+        if (root == null) {
             return null;
         }
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
         root.left = right;
         root.right = left;
+        return root;
+    }
+
+    public static TreeNode _invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
         return root;
     }
 }
