3997b2a87836048a1d36d8d91329da005dbfa9d5
闫飞 <583585752@qq.com>
2021-01-09 19:04:14 +0800

    # 203.移除链表元素

---
+++ src/com/yf/lintcode/ListNode.java
@@ -0,0 +1,10 @@
+package com.yf.lintcode;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/com/yf/lintcode/esay/RemoveElements203.java
@@ -0,0 +1,43 @@
+package com.yf.lintcode.esay;
+
+import com.yf.lintcode.ListNode;
+
+public class RemoveElements203 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next = new ListNode(6);
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        while (head != null && head.val == val) {
+            ListNode tmp = head;
+            head = head.next;
+            tmp.next = null;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+                ListNode tmp = cur;
+                cur = cur.next;
+                tmp.next = null;
+            } else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+
+}
